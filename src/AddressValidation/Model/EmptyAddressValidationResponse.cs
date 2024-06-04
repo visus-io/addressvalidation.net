644910a7d6125a3523c43f9d8c6cce3e24bb2d61
Alan Brault <alan.brault@visus.io>
2024-06-04 13:35:26 -0400

    task: remove duplicate code
    Signed-off-by: Alan Brault <alan.brault@visus.io>

--- src/AddressValidation/Model/EmptyAddressValidationResponse.cs
+++ src/AddressValidation/Model/EmptyAddressValidationResponse.cs
@@ -1,67 +1,17 @@
 namespace AddressValidation.Model;
 
-using System.Collections.ObjectModel;
 using System.Diagnostics.CodeAnalysis;
-using Abstractions;
-using FluentValidation;
 using FluentValidation.Results;
 
 /// <summary>
 ///     Represents an empty validation response
 /// </summary>
 [ExcludeFromCodeCoverage]
-public sealed class EmptyAddressValidationResponse : IAddressValidationResponse
+public sealed class EmptyAddressValidationResponse : AbstractAddressValidationResponse
 {
-	/// <summary>
-	///     Initializes a new instance of the <see cref="EmptyAddressValidationResponse" /> class.
-	/// </summary>
-	/// <param name="validationResult">
-	///     Current validation state of the response represented as an instance of
-	///     <see cref="ValidationResult" />.
-	/// </param>
-	public EmptyAddressValidationResponse(ValidationResult? validationResult)
+	/// <inheritdoc />
+	public EmptyAddressValidationResponse(ValidationResult? validationResult) 
+		: base(validationResult)
 	{
-		if ( validationResult is null )
-		{
-			return;
-		}
-
-		Errors = validationResult.Errors
-								  .Where(w => w.Severity == Severity.Error)
-								  .Select(s => !string.IsNullOrWhiteSpace(s.ErrorCode) ? $"{s.ErrorCode}: {s.ErrorMessage}" : $"{s.ErrorMessage}")
-								  .Distinct()
-								  .ToList()
-								  .AsReadOnly();
-
-		Warnings = validationResult.Errors
-									.Where(w => w.Severity == Severity.Warning)
-									.Select(s => !string.IsNullOrWhiteSpace(s.ErrorCode) ? $"{s.ErrorCode}: {s.ErrorMessage}" : $"{s.ErrorMessage}")
-									.Distinct()
-									.ToList()
-									.AsReadOnly();
 	}
-
-	/// <inheritdoc />
-	public IReadOnlyCollection<string> AddressLines => ReadOnlyCollection<string>.Empty;
-
-	/// <inheritdoc />
-	public string? CityOrTown => null;
-
-	/// <inheritdoc />
-	public CountryCode Country => CountryCode.ZZ;
-
-	/// <inheritdoc />
-	public IReadOnlyCollection<string> Errors { get; } = ReadOnlyObservableCollection<string>.Empty;
-
-	/// <inheritdoc />
-	public string? PostalCode => null;
-
-	/// <inheritdoc />
-	public string? StateOrProvince => null;
-
-	/// <inheritdoc />
-	public IReadOnlyCollection<IAddressValidationResponse> Suggestions => ReadOnlyCollection<IAddressValidationResponse>.Empty;
-
-	/// <inheritdoc />
-	public IReadOnlyCollection<string> Warnings { get; } = ReadOnlyObservableCollection<string>.Empty;
 }
